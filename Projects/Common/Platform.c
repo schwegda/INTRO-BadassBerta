d07df29f63844982bf1042aa5c9cbebb5494a4f3
Alex <schmidale@hotmail.com>
2015-03-02 09:55:03 +0100

    Added linked folder and platform.c and platform.h to the Robot project

---
+++ Projects/Common/Platform.c
@@ -0,0 +1,10 @@
+/*
+ * Platform.c
+ *
+ *  Created on: 02.03.2015
+ *      Author: Alexander
+ */
+
+
+
+
---
+++ Projects/Common/Platform.h
@@ -0,0 +1,15 @@
+/*
+ * Platform.h
+ *
+ *  Created on: 02.03.2015
+ *      Author: Alexander
+ */
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+
+
+
+
+#endif /* PLATFORM_H_ */
