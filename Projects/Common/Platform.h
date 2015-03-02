f4b013990528dbd8bf58d7dd572449df9cf49e96
David Schwegler <dave_sch_6@homtail.com>
2015-03-02 10:28:38 +0100

    Common: Add Board identification

--- Projects/Common/Platform.h
+++ Projects/Common/Platform.h
@@ -8,6 +8,15 @@
 #ifndef PLATFORM_H_
 #define PLATFORM_H_
 
+#ifdef PL_BOARD_IS_FRDM
+	#define PL_NOF_LEDS 3
+	/* FRDM Board has 3 LED's (RGB)*/
+#elif PL_BOARD_IS_ROBO
+	#define PL_NOF_LEDS 2
+	/*Robot has 2 LEDs*/
+#else
+	#error "This is not BadassBerta!!"
+#endif
 
 
 
