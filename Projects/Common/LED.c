1f15eab600eab9cd78c77d05d05c4474b2dee6a9
David Schwegler <dave_sch_6@homtail.com>
2015-03-03 11:36:02 +0100

    FRDM: LED.c Header added

--- Projects/Common/LED.c
+++ Projects/Common/LED.c
@@ -1,9 +1,31 @@
-/*
- * LED.c
- *
- *  Created on: 02.03.2015
- *      Author: Alexander
- */
+/* ###################################################################
+**     Filename    : LED.c
+**     Project     : INTRO
+**     Processor   : MK22FX512VLQ12 / MKL25Z128VLK4
+**     Version     : Driver 01.0
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2015-03-1, 08:21, # CodeGen: 0
+**     Abstract    :
+**         LED Interface implementation.
+**         This module Interface that make possible to use Code on Robot and FRDM Board
+**     Settings    :
+**     Contents    :
+**     		void LED_Init(void)
+**			void LED_Deinit(void)
+**
+** ###################################################################*/
+/*!
+** @file LED.c
+** @version 01.00
+** @brief
+**  	LED Interface implementation.
+**      This module Interface that make possible to use Code on Robot and FRDM Board.
+*/
+/*!
+**  @addtogroup LED_module LED module documentation
+**  @{
+*/
+/* MODULE Led */
 
 #include "LED.h"
 
@@ -22,4 +44,5 @@ void LED_Deinit(void) {
   /* nothing to do */
 }
 
-
+/*@}
+ */
