a14d1a1586b5d15acd560bddf31a1b6626714e30
David Schwegler <dave_sch_6@homtail.com>
2015-03-24 10:46:38 +0100

    FRDM: FreeRTOS Component

--- projects/FRDM/Sources/main.c
+++ projects/FRDM/Sources/main.c
@@ -69,6 +69,7 @@
 #include "UTIL1.h"
 #include "AS1.h"
 #include "ASerialLdd1.h"
+#include "FRTOS1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
